c4aa2668cdf377d564bd975f3c99bbf89e209871
Vignesh <vignesh.krishnaraj@entropiktech.com>
2023-10-22 18:25:52 +0530

    pessimistic and optimistic updates

--- src/App.js
+++ src/App.js
@@ -36,10 +36,16 @@ class App extends Component {
   };
 
   handleDelete = async (post) => {
+    const originalPosts = this.state.posts;
     console.log("Deleting post-id", post.id);
-    await axios.delete(apiEndpoint + "/" + post.id);
     const posts = this.state.posts.filter((p) => p.id !== post.id);
     this.setState({ posts });
+    try {
+      await axios.delete(apiEndpoint + "/" + post.id);
+    } catch (ex) {
+      alert("Something failed while deleting a post");
+      this.setState({ posts: originalPosts });
+    }
   };
 
   render() {
